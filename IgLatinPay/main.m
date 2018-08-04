181403ee49010f726b66677120a474fdd505576d
HighGroundLover <kclarkoneil@yahoo.ca>
2018-08-04 16:08:57 -0700

    Initial Commit

---
+++ IgLatinPay/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  IgLatinPay
+//
+//  Created by Kit Clark-O'Neil on 2018-08-04.
+//  Copyright © 2018 Kit Clark-O'Neil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
